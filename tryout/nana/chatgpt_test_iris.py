8feb8a47f7a7299c04d9328e7cd2c9b23d2deaa7
nanairis <103766808+nanairis@users.noreply.github.com>
2024-10-04 00:45:21 -0700

    Update chatgpt_test_iris.py
    rollback

--- tryout/nana/chatgpt_test_iris.py
+++ tryout/nana/chatgpt_test_iris.py
@@ -1,22 +1,17 @@
 from openai import OpenAI
 import openai
 import random
-import matplotlib.pyplot as plt
-import pandas as pd
-
 client = OpenAI(
     api_key="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",
 )
-
-
 def run_gpt(question_to_ask, model_type, key):
     llm_response = None
-    if model_type == "gpt-4o-mini" or model_type == "gpt-3.5-turbo":
+    if model_type == "gpt-4" or model_type == "gpt-3.5-turbo" :
         # Run OpenAI ChatCompletion API
         task = "Generate Python Code Script."
-        if model_type == "gpt-4o-mini":
+        if model_type == "gpt-4":
             # Ensure GPT-4 does not include additional comments
-            task = task + " The script should only include code, no comments. I need a executable code.Do not append any other can not execute text"
+            task = task + " The script should only include code, no comments."
         response = client.chat.completions.create(
             model=model_type,
             messages=[
@@ -36,14 +31,11 @@ def run_gpt(question_to_ask, model_type, key):
             presence_penalty=0.0
         )
         llm_response = response.choices[0].text
-
     else:
         print(f"Model '{model_type}' is not supported.")
         return ""
     return llm_response
-
-
-# get all available model of chat gpt
+#get all available model of chat gpt
 def get_available_models():
     try:
         models_response = client.models.list()
@@ -52,45 +44,9 @@ def get_available_models():
     except Exception as e:
         print("An error occurred while fetching models:", e)
         return []
-
-
-def getTemplateCode(df_dataset, df_name):
-   
-    primer_desc = "Use a dataframe called df from '../../project/EnhancedChat2VIS/data/movies.csv' with columns '" \
-                  + "','".join(str(x) for x in df_dataset.columns) + "'. "
-    for i in df_dataset.columns:
-        if len(df_dataset[i].drop_duplicates()) < 20 and df_dataset.dtypes[i] == "O":
-            primer_desc = primer_desc + "\nThe column '" + i + "' has categorical values '" + \
-                          "','".join(str(x) for x in df_dataset[i].drop_duplicates()) + "'. "
-        elif df_dataset.dtypes[i] == "int64" or df_dataset.dtypes[i] == "float64":
-            primer_desc = primer_desc + "\nThe column '" + i + "' is type " + str(
-                df_dataset.dtypes[i]) + " and contains numeric values. "
-    primer_desc = primer_desc + "\nLabel the x and y axes appropriately."
-    primer_desc = primer_desc + "\nAdd a title. Set the fig suptitle as empty."
-    primer_desc = primer_desc + "{}"  # Space for additional instructions if needed
-    primer_desc = primer_desc + "\nUsing Python version 3.9.12, create a script using the dataframe df to graph the following: "
-    pimer_code = "import pandas as pd\nimport matplotlib.pyplot as plt\n"
-    pimer_code = pimer_code + "fig,ax = plt.subplots(1,1,figsize=(10,4))\n"
-    pimer_code = pimer_code + "ax.spines['top'].set_visible(False)\nax.spines['right'].set_visible(False) \n"
-    pimer_code = pimer_code + "df=" + df_name + ".copy()\n"
-    return primer_desc, pimer_code
-
-
-# List to hold datasets
-datasets = {}
-# need to give exist path
-datasets["Movies"] = pd.read_csv("../../project/EnhancedChat2VIS/data/movies.csv")
-
-primer1, primer2 = getTemplateCode(datasets["Movies"], 'datasets["Movies"]')
-print(primer1)
-print(primer2)
-
-# use model to generate some code from gpt
-result = run_gpt(primer1 + primer2, "gpt-4o-mini", client.api_key)
+models=get_available_models()
+selected_model = random.choice(models)#get a random model include voice and picture and code model.
+print(selected_model)
+#use model to generate some code from gpt
+result = run_gpt("give me some code ,sum 2 number","gpt-4",client.api_key)
 print(result)
-
-#
-cleaned_code = result.lstrip('```python')
-cleaned_code = cleaned_code.rstrip('```')
-print(cleaned_code)
-exec(cleaned_code)
