a502bd623286dca056289bd15acafe11a95d91c2
lixiangjun <kerrylee000@gmail.com>
2024-10-03 22:08:34 -0700

    1. Change upload csv file into enter url 2. Modify local example dataset into GitHub dataset 3. Delete local example csv files

--- project/EnhancedChat2VIS/src/mainPage.py
+++ project/EnhancedChat2VIS/src/mainPage.py
@@ -2,8 +2,8 @@ import streamlit as st
 import os
 import pandas as pd
 from streamlit_option_menu import option_menu
-from uploadServer import *
 from loadpic_NL4DV import *
+import requests
 
 # The models that can be used, name: code
 availableLLM = {
@@ -12,7 +12,7 @@ availableLLM = {
     # "ChatGPT o1": "gptO1",
     "NL4DV 2.0": "nl4dv_2",
     "NL4DV 3.0": "nl4dv_3",
-    # "ncNET": "ncNet",
+    "Gemini": "gemini",
     "yoloPandas": "YOLOPandas"
 }
 
@@ -21,59 +21,66 @@ availableLLM = {
 exampleData = {
     "cars": "https://raw.githubusercontent.com/frog-land/Chat2VIS_Streamlit/main/cars.csv",
     "colleges": "https://raw.githubusercontent.com/frog-land/Chat2VIS_Streamlit/main/colleges.csv",
-    "customers_and_products_contacts": "https://raw.githubusercontent.com/frog-land/Chat2VIS_Streamlit/main/customers_and_products_contacts.csv",
-    "department_store": "https://raw.githubusercontent.com/frog-land/Chat2VIS_Streamlit/main/department_store",
+    "customers_and_products_contacts": "https://raw.githubusercontent.com/frog-land/Chat2VIS_Streamlit/main"
+                                       "/customers_and_products_contacts.csv",
+    "department_store": "https://raw.githubusercontent.com/frog-land/Chat2VIS_Streamlit/main/department_store.csv",
     "energy_production": "https://raw.githubusercontent.com/frog-land/Chat2VIS_Streamlit/main/energy_production.csv",
     "housing": "https://raw.githubusercontent.com/frog-land/Chat2VIS_Streamlit/main/housing.csv",
     "movies": "https://raw.githubusercontent.com/frog-land/Chat2VIS_Streamlit/main/movies.csv"
 }
 
 # OpenAI API key input
-apiKey = ''
-if 'apiKey' not in st.session_state:
-    st.session_state.apiKey = ''
+chatGptApiKey = ''
+if 'chatGptApiKey' not in st.session_state:
+    st.session_state.chatGptApiKey = ''
 else:
-    apiKey = st.session_state.apiKey
+    chatGptApiKey = st.session_state.chatGptApiKey
+
+geminiApiKey = ''
+if 'geminiApiKey' not in st.session_state:
+    st.session_state.geminiApiKey = ''
+else:
+    geminiApiKey = st.session_state.geminiApiKey
 
 # User choice of LLM usage
 llmChoice = {}
 
-# Start local server
-if 'server_started' not in st.session_state:
-    st.session_state.server_started = False
-
-if not st.session_state.server_started:
-    start_server()
-    st.session_state.server_started = True
-
 
 # API Key dialog
 @st.dialog("API key")
-def key(api):
+def key(api_openai, api_gemini):
     st.write("Enter OpenAI API Key")
-    apikeyInput = st.text_input("API Key", api)
+    chatGptApikeyInput = st.text_input("OpenAI API Key", api_openai)
+    st.write("")
+    st.write("Enter Gemini API Key")
+    geminiApikeyInput = st.text_input("Gemini API Key", api_gemini)
     if st.button('Submit'):  #
-        st.session_state.apiKey = apikeyInput
+        st.session_state.chatGptApiKey = chatGptApikeyInput
+        st.session_state.geminiApiKey = geminiApikeyInput
         st.rerun()
 
 
-# Preload example datasets
-dataLoc = '../data'
-fileNames = os.listdir(dataLoc)
-if 'exampleDataset' not in st.session_state:
-    exampleDataset = {}
-    for fileName in fileNames:
-        local_file_upload(f'{dataLoc}/{fileName}', "example")
-        exampleDataset[fileName.split('.')[0]] = pd.read_csv(f'{dataLoc}/{fileName}')
-    st.session_state.exampleDataset = exampleDataset
-else:
-    exampleDataset = st.session_state.exampleDataset
+# DEPRECATED: Using public URL instead of using local files.
+# Preload example datasets ()
+
+# dataLoc = '../data'
+# fileNames = os.listdir(dataLoc)
+# if 'exampleDataset' not in st.session_state:
+#     exampleDataset = {}
+#     for fileName in fileNames:
+#         local_file_upload(f'{dataLoc}/{fileName}', "example")
+#         exampleDataset[fileName.split('.')[0]] = pd.read_csv(f'{dataLoc}/{fileName}')
+#     st.session_state.exampleDataset = exampleDataset
+# else:
+#     exampleDataset = st.session_state.exampleDataset
+
 
 # Chosen file name
-chosenFile = ''
+chosenFileName = ''
+chosenFileURL = ''
 
 # Sidebar contents
-with st.sidebar:
+with (st.sidebar):
     st.title("Enhanced Chat2VIS")
     col1, col2 = st.columns([2, 1])  # Adjust the ratio for better alignment
 
@@ -82,62 +89,100 @@ with st.sidebar:
 
     # API key button
     with col2:
-        if st.button('Key ✅' if st.session_state.apiKey != '' else 'API Key'):
-            key(st.session_state.apiKey)
-            apiKey = st.session_state.apiKey
+        if st.button('Key ✅' if (st.session_state.chatGptApiKey != '' and st.session_state.geminiApiKey != '')
+                     else 'API Key'):
+            key(st.session_state.chatGptApiKey, st.session_state.geminiApiKey)
+            chatGptApiKey = st.session_state.chatGptApiKey
+            geminiApiKey = st.session_state.geminiApiKey
 
     # Model selection checkboxes
     for llmName, llmCode in availableLLM.items():
-        llmChoice[llmCode] = st.checkbox(llmName)
+        if (('ChatGPT' in llmName and chatGptApiKey == "") or
+                ('yolo' in llmName and chatGptApiKey == "") or ('NL4DV 2.0' in llmName and chatGptApiKey == "")):
+            llmChoice[llmCode] = st.checkbox(llmName, disabled=True, help="Please enter ChatGPT API key first.")
+        elif 'Gemini' in llmName and geminiApiKey == "":
+            llmChoice[llmCode] = st.checkbox(llmName, disabled=True, help="Please enter Gemini API key first.")
+        else:
+            llmChoice[llmCode] = st.checkbox(llmName)
 
     # Dataset source selection
     st.markdown("### Choose your dataset")
-    datasetSelected = option_menu(None, ['Example', 'Upload'],
-                                  icons=['clipboard-data-fill', 'file-earmark-arrow-up-fill'], orientation="horizontal")
 
-    # Example dataset selection
-    if datasetSelected == "Example":
+    # Choose
+    selected = option_menu(None, ['Custom', 'Example'],
+                           icons=['file-earmark-arrow-up-fill', 'clipboard-data-fill'],
+                           orientation="horizontal")
+
+    # Show example dataset
+    if selected == 'Example':
         st.session_state['isExampleDataset'] = True
         exampleDataChosen = st.radio(
-            "Choose an example dataset", [key.capitalize() for key in exampleDataset.keys()]
+            "Choose an example dataset", [key.capitalize() for key in exampleData.keys()]
         )
-        chosenFile = exampleDataChosen.lower() + ".csv"
-        print(chosenFile)
+        chosenFileName = exampleDataChosen.lower()
+        chosenFileURL = exampleData[chosenFileName]
+        print(chosenFileName, chosenFileURL)
+
+    # Show upload dataset
     else:
         st.session_state['isExampleDataset'] = False
-        chosenFile = ''
-        # Upload a CSV file
-        uploadDataset = st.file_uploader("Please upload a CSV file", type="csv")
-        if uploadDataset is not None:
-            uploadFileName = uploadDataset.name  # Get upload file name
+        # 初始化session_state
+        if 'customFileURL' not in st.session_state:
+            st.session_state['customFileURL'] = ''
+            st.session_state['csv_checked'] = False  # To mark whether CONFIRM button is checked.
 
-            # Upload data to local server
-            local_file_upload(uploadDataset, 'upload')
+        # URL input, if checked, disabled input.
+        if not st.session_state['csv_checked']:
+            inputURL = st.text_input("Input a custom CSV file URL", value=st.session_state['customFileURL'])
 
-            # Store uploaded dataset in session state
-            st.session_state.uploaded_df = pd.read_csv(uploadDataset)
-            chosenFile = uploadFileName
+            # confirm button, click to check url
+            if st.button("Confirm"):
+                if inputURL.endswith(".csv"):
+                    st.session_state['customFileURL'] = inputURL
+                    st.session_state['csv_checked'] = True
+                    st.rerun()
+                else:
+                    st.error("ERROR: Please provide a legal .csv URL")
+        else:
+            st.text_input("Input a custom CSV file URL", value=st.session_state['customFileURL'], disabled=True)
+            st.success(".csv file checked")
+            chosenFileName = st.session_state['customFileURL'].split('/')[-1].replace('.csv', '')
+            chosenFileURL = st.session_state['customFileURL']
+            print(chosenFileName, chosenFileURL)
+
+            # Edit button
+            if st.button("Edit"):
+                st.session_state['csv_checked'] = False
+                st.rerun()
 
 # Text area for input commands
 st.markdown("## What would you like to visualize?")
-user_input = st.text_area("👀 What would you like to visualize?")
+user_input = st.text_area("Input prompts")
 
 # Button to run LLM
 if st.button('Go...', key='submit'):
-    if chosenFile == '':
+    if chosenFileName == '':
         st.write("Please upload a CSV file")
     else:
         # Placeholder for LLM processing (to be implemented)
         st.write(f"Running model on: {user_input}")
         if llmChoice["nl4dv_3"]:
             print("running nl4dv_3")
-            st.vega_lite_chart(use_nl4dv_3(f"http://localhost:8000/{chosenFile}", apiKey, user_input))
+            st.vega_lite_chart(use_nl4dv_3(chosenFileURL, chatGptApiKey, user_input))
 
 # Display dataset as a table
 if 'isExampleDataset' in st.session_state and st.session_state['isExampleDataset']:
-    if exampleDataChosen:
-        st.markdown(f"### {exampleDataChosen} Dataset")
-        st.dataframe(exampleDataset[exampleDataChosen.lower()])
-elif 'uploaded_df' in st.session_state:
-    st.markdown(f"### Uploaded Dataset: {uploadFileName}")
-    st.dataframe(st.session_state.uploaded_df)
+    if chosenFileName:
+        st.markdown(f"### {chosenFileName} Dataset")
+        try:
+            csv = pd.read_csv(chosenFileURL)
+            st.dataframe(csv)
+        except Exception as e:
+            st.error(f"Can't read CSV file: {e}")
+elif 'csv_checked' in st.session_state and st.session_state['csv_checked']:
+    st.markdown(f"### Custom Dataset: {chosenFileName}")
+    try:
+        csv = pd.read_csv(chosenFileURL)
+        st.dataframe(csv)
+    except Exception as e:
+        st.error(f"Can't read CSV file: {e}")
--- tryout/Kerry/NL4DV/src/testWeb.py
+++ tryout/Kerry/NL4DV/src/testWeb.py
@@ -16,7 +16,30 @@ dt = {"query": "show average MPG of different Origin", "query_raw": "show averag
              "operator": "AVG", "inferenceType": "explicit"}]}, "visType": "None", "visQueryPhrase": "None",
       "dialogId": "0", "queryId": "0"}
 
-dt2 = {'query_raw': 'show average MPG of different Origin', 'query': 'show average mpg of different origin', 'dataset': '../resource/cars.csv', 'alias': None, 'visList': [{'attributes': ['MPG', 'Origin'], 'queryPhrase': None, 'visType': None, 'tasks': ['derived_value'], 'inferenceType': 'implicit', 'vlSpec': {'$schema': 'https://vega.github.io/schema/vega-lite/v4.json', 'mark': {'type': 'bar', 'tooltip': True}, 'encoding': {'y': {'field': 'MPG', 'type': 'quantitative', 'aggregate': 'mean', 'axis': {'format': 's'}}, 'x': {'field': 'Origin', 'type': 'nominal'}}, 'transform': [], 'data': {'url': '../resource/cars.csv', 'format': {'type': 'csv'}}}}], 'attributeMap': {'MPG': {'name': 'MPG', 'queryPhrase': ['mpg'], 'inferenceType': 'explicit', 'isAmbiguous': False, 'ambiguity': []}, 'Origin': {'name': 'Origin', 'queryPhrase': ['origin'], 'inferenceType': 'explicit', 'isAmbiguous': False, 'ambiguity': []}}, 'taskMap': {'derived_value': [{'task': 'derived_value', 'queryPhrase': 'average', 'operator': 'AVG', 'values': [], 'attributes': ['MPG'], 'inferenceType': 'explicit', 'followup_type': 'nothing'}]}, 'followUpQuery': False, 'contextObj': None, 'attributeMapping': {'MPG': {'mpg': 1}, 'Origin': {'origin': 1}}, 'followUpConfidence': None, 'ambiguity': {'attribute': {}, 'value': {}}, 'dialogId': '0', 'queryId': '0'}
+dt2 = {'query_raw': 'show average MPG of different Origin', 'query': 'show average mpg of different origin',
+       'dataset': 'https://raw.githubusercontent.com/frog-land/Chat2VIS_Streamlit/main/cars.csv', 'alias': None, 'visList': [
+        {'attributes': ['MPG', 'Origin'], 'queryPhrase': None, 'visType': None, 'tasks': ['derived_value'],
+         'inferenceType': 'implicit', 'vlSpec': {'$schema': 'https://vega.github.io/schema/vega-lite/v4.json',
+                                                 'mark': {'type': 'bar', 'tooltip': True}, 'encoding': {
+                'y': {'field': 'MPG', 'type': 'quantitative', 'aggregate': 'mean', 'axis': {'format': 's'}},
+                'x': {'field': 'Origin', 'type': 'nominal'}}, 'transform': [],
+                                                 'data': {'url': 'https://raw.githubusercontent.com/frog-land/Chat2VIS_Streamlit/main/cars.csv', 'format': {'type': 'csv'}}}}],
+       'attributeMap': {
+           'MPG': {'name': 'MPG', 'queryPhrase': ['mpg'], 'inferenceType': 'explicit', 'isAmbiguous': False,
+                   'ambiguity': []},
+           'Origin': {'name': 'Origin', 'queryPhrase': ['origin'], 'inferenceType': 'explicit', 'isAmbiguous': False,
+                      'ambiguity': []}}, 'taskMap': {'derived_value': [
+        {'task': 'derived_value', 'queryPhrase': 'average', 'operator': 'AVG', 'values': [], 'attributes': ['MPG'],
+         'inferenceType': 'explicit', 'followup_type': 'nothing'}]}, 'followUpQuery': False, 'contextObj': None,
+       'attributeMapping': {'MPG': {'mpg': 1}, 'Origin': {'origin': 1}}, 'followUpConfidence': None,
+       'ambiguity': {'attribute': {}, 'value': {}}, 'dialogId': '0', 'queryId': '0'}
 
-st.vega_lite_chart(dt2["visList"][0]["vlSpec"])
+st.write("show average mpg of different origin")
+col1, col2 = st.columns([2, 1])
+with col1:
+    st.write("NL4DV 3.0")
+    st.vega_lite_chart(dt["visList"][0]["vlSpec"])
+with col2:
+    st.write("NL4DV 2.0")
+    st.vega_lite_chart(dt2["visList"][0]["vlSpec"])
 
