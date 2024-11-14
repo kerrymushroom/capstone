69919069bdfd164a66896c1e91ee946516087a47
Weynie <waeynne@gmail.com>
2024-11-13 18:56:23 -0800

    Add getData method

---
+++ findRule/getData.py
@@ -0,0 +1,13 @@
+import re
+
+include = [r'\n(\w+).plot\(',r'\n(?:ax|plt).\w+.(\w+)[.[]{1}']
+text = ""
+
+# get the target dataframe string
+def get_dataframe(text,include):
+    res = ""
+    for line in include:
+        if  re.search(line, text):
+            res = re.search(line, text).group(1)
+            break
+    return res
