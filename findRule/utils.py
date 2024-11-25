63e42544db029868368d264475e0df7093eddd66
Weynie <waeynne@gmail.com>
2024-11-25 14:00:35 -0800

    add geminiTest code

--- findRule/utils.py
+++ findRule/utils.py
@@ -35,19 +35,18 @@ def mask(func, *args, **kwargs):
     matplotlib.use(original_backend)
     #matplotlib.use('module://matplotlib_inline.backend_inline')
 
-
 def save_df_json(df):
     # convert non-numeric index class such as (pd.DatetimeIndex, pd.PeriodIndex,TimedeltaIndex,IntervalIndex) to string
     if not (isinstance(df.index, pd.Index) and df.index.dtype in ['int64', 'float64']):
         df.index = [str(idx) for idx in df.index]
     # if the index is integer number, then the index is not x_data, the first column is x_data, the rest columns are y_data
-    if df.index[1]==1:
-        x_data = [df[df.columns[0]].tolist()]
-        y_data = [df[col].tolist() for col in df.columns[1:]] 
-    elif isinstance(df, pd.Series):
+    if isinstance(df, pd.Series):
          # for Series object, only one dimention
         x_data = [df.index.tolist()]
         y_data = [df.values.tolist()]
+    elif df.index[1]==1:
+        x_data = [df[df.columns[0]].tolist()]
+        y_data = [df[col].tolist() for col in df.columns[1:]] 
     else:
         # else, the index is x_data, all the columns is y_data
         x_data = [df.index.tolist()]
